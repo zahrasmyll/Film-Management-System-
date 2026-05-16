9c12acea68c9e2c50b815d4f6fecb63334269aaa
zahrasmyll <zulyaismailova04@gmail.com>
2026-05-16 21:36:33 +0300

    Create ReviewNode.java

---
+++ src/ReviewNode.java
@@ -0,0 +1,12 @@
+package com.mycompany.filmmanagement;
+
+
+public class reviewnode {
+    Review data;
+    Node next;
+
+    public reviewnode(Review data) {
+        this.data = data;
+        this.next = null;
+    }
+}
