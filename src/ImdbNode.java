9685f34637af7070585e47c2bd212895729f38db
zahrasmyll <zulyaismailova04@gmail.com>
2026-05-16 21:31:53 +0300

    Create ImdbNode.java

---
+++ src/ImdbNode.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.filmmanagement;
+
+
+public class ImdbNode {
+    double imdb ; 
+    ImdbNode next;
+    
+    public ImdbNode(double imdb){
+        this.imdb = imdb;
+        next = null;
+    }
+
+}
